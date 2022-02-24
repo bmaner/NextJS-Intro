ffbc8c1a7fde6c41f0cc4ce3f29c9b55f722d920
bmaner <bmanerdaniel@gmail.com>
2022-02-24 15:34:10 +0900

    Fix from CSR to SSR

--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,15 @@
 import { useEffect, useState } from 'react';
 import Seo from '../components/Seo';
 
-export default function Home() {
+export default function Home({ results }) {
   const [movies, setMovies] = useState();
 
-  useEffect(() => {
-    (async () => {
-      const response = await fetch(`/api/movies`);
-      const { results } = await response.json();
-      console.log(results);
-      setMovies(results);
-    })();
-  }, []);
+  // useEffect(() => {
+  //   (async () => {
+  //     // const { results } = await (await fetch(`/api/movies`)).json();
+  //     setMovies(results);
+  //   })();
+  // }, []);
   //   const [counter, setCounter] = useState(0);
   return (
     <div className="container">
@@ -26,8 +24,8 @@ export default function Home() {
       {/* <h1>Hello! World!</h1> */}
       {/* <h1>Hello World!{counter}</h1> */}
       {/* <button onClick={() => setCounter(counter => counter + 1)}>+</button> */}
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map(movie => (
+      {/* {!movies && <h4>Loading...</h4>} */}
+      {results?.map(movie => (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -56,4 +54,15 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
